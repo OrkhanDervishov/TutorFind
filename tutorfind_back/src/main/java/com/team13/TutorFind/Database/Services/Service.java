b6985e577507d25262c9d9dd5b102a31b9e63b68
Turgay Ibrahimli <turqayibrahimli1@gmail.com>
2025-12-16 02:25:55 +0400

    Added environmental libraries for better usage

--- tutorfind_back/src/main/java/com/team13/TutorFind/Database/Services/Service.java
+++
@@ -1,5 +0,0 @@
-package com.team13.TutorFind.Database.Services;
-
-public interface Service {
-    void save();
-}
--- tutorfind_back/src/main/java/com/team13/TutorFind/User/Learner.java
+++
@@ -1,8 +0,0 @@
-package com.team13.TutorFind.User;
-
-public class Learner extends User{
-
-    Learner(){
-        super();
-    }
-}
